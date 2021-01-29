c4d794172530232306e35de18975163af27eab98
Istomina Asya <istomina.asia@yandex.ru>
2021-01-30 00:56:13 +0300

    02 03 MeetupsCalendar in progress -made rendering correct calendar cells

--- 02-components/03-MeetupsCalendar/MeetupsCalendar.js
+++ 02-components/03-MeetupsCalendar/MeetupsCalendar.js
@@ -1,6 +1,8 @@
 /*
   Полезные функции по работе с датой можно описать вне Vue компонента
  */
+const _DAY = 3600000 * 24;
+
 
 export const MeetupsCalendar = {
   name: 'MeetupsCalendar',
@@ -10,22 +12,13 @@ export const MeetupsCalendar = {
       <div class="rangepicker__month-indicator">
         <div class="rangepicker__selector-controls">
           <button class="rangepicker__selector-control-left" @click="rewindMonth"></button>
-          <div>Июнь 2020</div>
+          <div>{{ month }}</div>
           <button class="rangepicker__selector-control-right" @click="forwardMonth"></button>
         </div>
       </div>
+      <div>{{ date }}</div>
       <div class="rangepicker__date-grid">
-        <div class="rangepicker__cell rangepicker__cell_inactive">28</div>
-        <div class="rangepicker__cell rangepicker__cell_inactive">29</div>
-        <div class="rangepicker__cell rangepicker__cell_inactive">30</div>
-        <div class="rangepicker__cell rangepicker__cell_inactive">31</div>
-        <div class="rangepicker__cell">
-          1
-          <a class="rangepicker__event">Митап</a>
-          <a class="rangepicker__event">Митап</a>
-        </div>
-        <div class="rangepicker__cell">2</div>
-        <div class="rangepicker__cell">3</div>
+      <div class="rangepicker__cell rangepicker__cell_inactive" v-for="day in days">{{ day.num }}</div>
       </div>
     </div>
   </div>`,
@@ -42,7 +35,7 @@ export const MeetupsCalendar = {
     }
   },
   mounted() {
-    this.date = new Date();
+    this.date = new Date((new Date()).setDate(1));
   },
 
 
@@ -51,15 +44,59 @@ export const MeetupsCalendar = {
   // Изначально должен показываться текущий месяц
 
   // Вычислимые свойства помогут как с получением списка дней, так и с выводом информации
+  computed: {
+    month() {
+      return this.date? this.date.toLocaleString(navigator.language, {
+        year: 'numeric',
+        month: 'long',
+      }) : "";
+    },
+    days() {
+     let result = [];
+      let _current = new Date(this.date);
+      if (!_current) return result;
+      let _month = _current.getMonth();
+      let _week = _current.getDay() ? _current.getDay() - 1 : 6;
+      _current = new Date(_current.setDate(_current.getDate() - _week));
+      let _daysInMonth = new Date((new Date((new Date(_current)).setMonth(_month + 1))).setDate(0)).getDate();
+      let _rest = (_daysInMonth + _week) % 7 ? 7 - ((_daysInMonth + _week) % 7) : 0;
+      let cells = _daysInMonth + _week + _rest;
+
+      for (let cell = 1; cell <= cells; cell++) {
+        let day = {
+          num: _current.getDate(),
+          date: _current,
+        }
+        _current =  new Date(_current.setDate(_current.getDate() + 1))
+        result.push(day);
+      }
+      return result;
+    }
+
+  },
 
   methods: {
     rewindMonth() {
-      this.date = new Date(this.date.setMonth(this.date.getMonth() - 1));
-      alert(this.date.getMonth());
+      let _date = new Date(this.date);
+      let _currentMonth = _date.getMonth();
+      this.date = new Date(_date.setMonth(_currentMonth - 1));
     },
     forwardMonth() {
-      this.date = new Date(this.date.setMonth(this.date.getMonth() + 1));
-      alert(this.date);
+      let _date = new Date(this.date);
+      let _currentMonth = _date.getMonth();
+      this.date = new Date(_date.setMonth(_currentMonth + 1));
+
     }
   },
 };
+/* <div class="rangepicker__cell rangepicker__cell_inactive">28</div>
+        <div class="rangepicker__cell rangepicker__cell_inactive">29</div>
+        <div class="rangepicker__cell rangepicker__cell_inactive">30</div>
+        <div class="rangepicker__cell rangepicker__cell_inactive">31</div>
+        <div class="rangepicker__cell">
+          1
+          <a class="rangepicker__event">Митап</a>
+          <a class="rangepicker__event">Митап</a>
+        </div>
+        <div class="rangepicker__cell">2</div>
+        <div class="rangepicker__cell">3</div> */

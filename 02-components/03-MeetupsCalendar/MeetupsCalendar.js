e198fb8ebfbd0720f2914f997b9d8e1c46be68b7
Istomina Asya <istomina.asia@yandex.ru>
2021-01-30 08:50:30 +0300

    02 03 MeetupsCalendar in progress -made minimum refactor

--- 02-components/03-MeetupsCalendar/MeetupsCalendar.js
+++ 02-components/03-MeetupsCalendar/MeetupsCalendar.js
@@ -1,8 +1,3 @@
-/*
-  Полезные функции по работе с датой можно описать вне Vue компонента
- */
-const _DAY = 3600000 * 24;
-
 
 export const MeetupsCalendar = {
   name: 'MeetupsCalendar',
@@ -16,9 +11,8 @@ export const MeetupsCalendar = {
           <button class="rangepicker__selector-control-right" @click="forwardMonth"></button>
         </div>
       </div>
-      <div>{{ date }}</div>
       <div class="rangepicker__date-grid">
-      <div class="rangepicker__cell rangepicker__cell_inactive" v-for="day in days">{{ day.num }}
+      <div class="rangepicker__cell" :class="{'rangepicker__cell_inactive' : !day.isActive}" v-for="day in days">{{ day.num }}
         <template v-if="day.events">
           <a class="rangepicker__event" v-for="event in day.events">{{ event }}</a>
         </template>
@@ -35,36 +29,34 @@ export const MeetupsCalendar = {
       required: true,
     }
   },
+
   data() {
     return {
       date: null,
     }
   },
+
   mounted() {
     this.date = new Date((new Date()).setDate(1));
     this.computeShedule();
-    console.log(Object.keys(this.$options.shedule));
-    this.meetups.forEach(meetup => console.log((new Date(meetup.date)).toDateString()));
   },
+
   updated() {
     this.computeShedule();
   },
 
-
-  // В качестве локального состояния требуется хранить что-то,
-  // что позволит определить текущий показывающийся месяц.
-  // Изначально должен показываться текущий месяц
-
-  // Вычислимые свойства помогут как с получением списка дней, так и с выводом информации
   computed: {
+
     month() {
       return this.date? this.date.toLocaleString(navigator.language, {
         year: 'numeric',
         month: 'long',
       }) : "";
     },
+
     days() {
-     let result = [];
+
+      let result = [];
       let _current = new Date(this.date);
       if (!_current) return result;
       let _month = _current.getMonth();
@@ -78,27 +70,31 @@ export const MeetupsCalendar = {
         let day = {
           num: _current.getDate(),
           date: _current,
+          isActive: true,
         }
+        if (_current.getMonth() != _month) day.isActive = false;
         if (this.$options.shedule[_current.toDateString()]) day['events'] = this.$options.shedule[_current.toDateString()];
         _current =  new Date(_current.setDate(_current.getDate() + 1))
         result.push(day);
       }
       return result;
     }
-
   },
 
   methods: {
+
     rewindMonth() {
       let _date = new Date(this.date);
       let _currentMonth = _date.getMonth();
       this.date = new Date(_date.setMonth(_currentMonth - 1));
     },
+
     forwardMonth() {
       let _date = new Date(this.date);
       let _currentMonth = _date.getMonth();
       this.date = new Date(_date.setMonth(_currentMonth + 1));
     },
+
     computeShedule() {
       this.$options.shedule = {};
       this.meetups.forEach(meetup => {
@@ -110,14 +106,4 @@ export const MeetupsCalendar = {
     }
   },
 };
-/* <div class="rangepicker__cell rangepicker__cell_inactive">28</div>
-        <div class="rangepicker__cell rangepicker__cell_inactive">29</div>
-        <div class="rangepicker__cell rangepicker__cell_inactive">30</div>
-        <div class="rangepicker__cell rangepicker__cell_inactive">31</div>
-        <div class="rangepicker__cell">
-          1
-          <a class="rangepicker__event">Митап</a>
-          <a class="rangepicker__event">Митап</a>
-        </div>
-        <div class="rangepicker__cell">2</div>
-        <div class="rangepicker__cell">3</div> */
+

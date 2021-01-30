878153a155e567792c3a1bf2dd4f2661ab1e6320
Istomina Asya <istomina.asia@yandex.ru>
2021-01-30 13:00:40 +0300

    02 03 MeetupsCalendar completed

--- 02-components/03-MeetupsCalendar/MeetupsCalendar.js
+++ 02-components/03-MeetupsCalendar/MeetupsCalendar.js
@@ -1,3 +1,44 @@
+/**
+ * Возвращает следующий день после текущего
+ * @param date {Date} - текущая дата
+ * @return {Date} - следующий день
+ */
+function setNextDay(date) {
+  return new Date(date.setDate(date.getDate() + 1))
+}
+
+/**
+ * Возвращает первый день для отобра;ения в текущем представлении сетки
+ * @param date {Date} - первый день текущего месяца
+ * @return {Date} - объект даты
+ */
+function getFirstInGrid(date) {
+  let day = date.getDay() ? date.getDay() - 1 : 6; //день недели, с которого начинается месяц
+  return new Date(date.setDate(date.getDate() - day));
+}
+
+/**
+ * Возвращает сколько всего дней дол;но быть в сетке
+ * @param date - первый день текущего месяца
+ * @return {Number} - количество ячеек в сетке
+ */
+function getTotalDays(date) {
+  let month = date.getMonth(); //текущий месяц
+  let day = date.getDay() ? date.getDay() - 1 : 6; //день недели, с которого начинается месяц
+  let _daysInMonth = new Date((new Date((new Date(date)).setMonth(month + 1))).setDate(0)).getDate();
+  let _rest = (_daysInMonth + day) % 7 ? 7 - ((_daysInMonth + day) % 7) : 0;
+  let total = _daysInMonth + day + _rest;
+  return total;
+}
+
+/**
+ * Возвращает строку даты без часов / минут
+ * @param date {Date} - объект даты
+ * @return {string} - строка даты
+ */
+function makeDateString(date) {
+  return (new Date(date)).toDateString();
+}
 
 export const MeetupsCalendar = {
   name: 'MeetupsCalendar',
@@ -21,7 +62,6 @@ export const MeetupsCalendar = {
     </div>
   </div>`,
 
-
   props: {
     meetups: {
       type: Array,
@@ -37,18 +77,20 @@ export const MeetupsCalendar = {
 
   computed: {
 
+    //митапы отсортированные по датам (только названия)
     shedule() {
       let shedule = {};
       this.meetups.forEach(meetup => {
-        if (!shedule[(new Date(meetup.date)).toDateString()]) {
-          shedule[(new Date(meetup.date)).toDateString()] = [];
+        let _date = makeDateString(meetup.date);
+        if (!shedule[_date]) {
+          shedule[_date] = [];
         }
-        shedule[(new Date(meetup.date)).toDateString()].push(meetup.title)
+        shedule[_date].push(meetup.title)
       });
-      console.log('okkk');
       return shedule;
     },
 
+    //текущий месяц в строковом формате
     month() {
       return this.date? this.date.toLocaleString(navigator.language, {
         year: 'numeric',
@@ -56,28 +98,30 @@ export const MeetupsCalendar = {
       }) : "";
     },
 
+    //данные для ячеек сетки
     days() {
 
       let result = [];
-      let _current = new Date(this.date);
-      if (!_current) return result;
-      let _month = _current.getMonth();
-      let _week = _current.getDay() ? _current.getDay() - 1 : 6;
-      _current = new Date(_current.setDate(_current.getDate() - _week));
-      let _daysInMonth = new Date((new Date((new Date(_current)).setMonth(_month + 1))).setDate(0)).getDate();
-      let _rest = (_daysInMonth + _week) % 7 ? 7 - ((_daysInMonth + _week) % 7) : 0;
-      let cells = _daysInMonth + _week + _rest;
+
+      let _current = new Date(this.date); //первый день текущего месяца
+      let _month = _current.getMonth(); //текущий месяц
+
+      _current = getFirstInGrid(_current); //понедельник первой недели месяца
+
+      let cells = getTotalDays(_current); //сколько всего ячеек в сетке текущего месяца
 
       for (let cell = 1; cell <= cells; cell++) {
         let day = {
           num: _current.getDate(),
-          date: _current,
           isActive: true,
         }
-        if (_current.getMonth() != _month) day.isActive = false;
-        if (this.shedule[_current.toDateString()]) day['events'] = this.shedule[_current.toDateString()];
-        _current =  new Date(_current.setDate(_current.getDate() + 1))
-        result.push(day);
+        if (_current.getMonth() != _month) day.isActive = false; //если не текущий месяц, ячейки неактивные
+
+        let _date = makeDateString(_current); //проверка, есть ли мероприятия
+        if (this.shedule[_date]) day['events'] = this.shedule[_date];
+
+        result.push(day); //запись данных ячейки в результат
+        _current =  setNextDay(_current); //переход на следующую итерацию
       }
       return result;
     }
@@ -97,7 +141,6 @@ export const MeetupsCalendar = {
       this.date = new Date(_date.setMonth(_currentMonth + 1));
     },
   },
-
 
 };
 

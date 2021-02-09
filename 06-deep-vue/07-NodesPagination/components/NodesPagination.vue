d18769c0f503a1a12f546cef91079b7b204bd97f
Istomina Asya <istomina.asia@yandex.ru>
2021-02-09 11:50:19 +0300

    06 07 NodesPagination in progress -added function calc

--- 06-deep-vue/07-NodesPagination/components/NodesPagination.vue
+++ 06-deep-vue/07-NodesPagination/components/NodesPagination.vue
@@ -1,6 +1,24 @@
 <script>
+function calc(per, page) {
+  return Array.from({ length: per }, (v, k) => k + 1 + per * (page - 1));
+}
 export default {
   name: 'NodesPagination',
+  render: function (createElement) {
+    for (let i of calc(this['per-page'], this.page)) {
+      return createElement('slot');
+    }
+  },
+  props: {
+    page: {
+      type: Number,
+      default: 1,
+    },
+    'per-page': {
+      type: Number,
+      required: true,
+    },
+  },
 };
 </script>
 

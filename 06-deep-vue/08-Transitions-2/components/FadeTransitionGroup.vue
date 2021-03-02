37e94098085aa8632fb2e3a21bfcae6f8f57a517
Istomina Asya <istomina.asia@yandex.ru>
2021-03-02 20:24:12 +0300

    06 08 Transitions-2 -completed

--- 06-deep-vue/08-Transitions-2/components/FadeTransitionGroup.vue
+++ 06-deep-vue/08-Transitions-2/components/FadeTransitionGroup.vue
@@ -1,6 +1,50 @@
 <script>
+/*  */
 export default {
   name: 'FadeTransitionGroup',
+
+  render: function (createElement) {
+    let a = createElement('p', '');
+    let VNode = a.__proto__.constructor;
+    function cloneVNode(vnode) {
+      const cloned = new VNode(
+        vnode.tag,
+        vnode.data,
+        // #7975
+        // clone children array to avoid mutating original in case of cloning
+        // a child.
+        vnode.children && vnode.children.slice(),
+        vnode.text,
+        vnode.elm,
+        vnode.context,
+        vnode.componentOptions,
+        vnode.asyncFactory,
+      );
+      cloned.ns = vnode.ns;
+      cloned.isStatic = vnode.isStatic;
+      cloned.key = vnode.key;
+      cloned.isComment = vnode.isComment;
+      cloned.fnContext = vnode.fnContext;
+      cloned.fnOptions = vnode.fnOptions;
+      cloned.fnScopeId = vnode.fnScopeId;
+      cloned.asyncMeta = vnode.asyncMeta;
+      cloned.isCloned = true;
+      return cloned;
+    }
+    let transitionGroup = cloneVNode(
+      createElement(
+        'transition-group',
+        { attrs: Object.assign({ name: 'fade-list' }, this.$attrs) },
+        this.$slots.default,
+      ),
+    );
+    let slots_ = transitionGroup.context.$slots.default;
+    for (let slot of slots_) {
+      slot.data.class = 'fade-list-item';
+    }
+    transitionGroup.data.class = 'fade-list';
+    return transitionGroup;
+  },
 };
 </script>
 

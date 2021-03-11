dcd34ad05c21ce15bdc1a22e3d292e2932a2b081
javascriptru <67475495+jsru-1@users.noreply.github.com>
2021-03-11 21:03:46 +0300

    Added task CalendarView

---
+++ 08-interactions-and-logic-reuse/03-CalendarView/views/MeetupsPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <meetups-calendar v-if="meetups" :meetups="meetups" />
+</template>
+
+<script>
+import MeetupsCalendar from '../components/MeetupsCalendar';
+import { fetchMeetups } from '../data';
+
+export default {
+  name: 'MeetupsPage',
+  components: { MeetupsCalendar },
+
+  data() {
+    return {
+      meetups: null,
+    };
+  },
+
+  async mounted() {
+    this.meetups = await fetchMeetups();
+  },
+};
+</script>
+
+<style></style>

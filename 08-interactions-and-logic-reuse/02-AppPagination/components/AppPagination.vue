2fd22bf572ab6488648a8e00994d6d6b9d9f5b65
javascriptru <67475495+jsru-1@users.noreply.github.com>
2021-03-12 16:02:55 +0300

    Added teacher solution for task: AppPagination

---
+++ 08-interactions-and-logic-reuse/02-AppPagination/components/AppPagination.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <!-- Используем template для повторения слота для всех элементов текущей страницы -->
+    <template v-for="item in pageItems">
+      <!-- В слот передаём один элемент с текущей страницы -->
+      <slot :item="item" />
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppPagination',
+
+  props: {
+    page: Number,
+    perPage: Number,
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  computed: {
+    pageItems() {
+      // Вычисляемым свойством находим все элементы текущей страницы
+      const start = (this.page - 1) * this.perPage;
+      const end = this.page * this.perPage;
+      return this.items.slice(start, end);
+    },
+  },
+};
+</script>
+
+<style></style>

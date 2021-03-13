96f515776966aa4e148f38a72a68d61291c38ae6
Istomina Asya <istomina.asia@yandex.ru>
2021-03-13 13:03:05 +0300

    10 01 RenderlessCalendar -completed

--- 10-architeture/01-RenderlessCalendar/components/MeetupsCalendar.vue
+++ 10-architeture/01-RenderlessCalendar/components/MeetupsCalendar.vue
@@ -1,18 +1,24 @@
 <template>
-  <calendar-view>
-    <!--
-    <calendar-view-event
-      tag="router-link"
-      :to="{ name: 'meetup', params: { meetupId: meetup.id } }"
-      >{{ meetup.title }}</calendar-view-event
-    >
-    -->
+  <calendar-view v-slot="{ date }">
+    <template v-if="shedule[date]">
+      <calendar-view-event
+        v-for="meetup in shedule[date]"
+        :key="meetup.id"
+        tag="router-link"
+        :to="{ name: 'meetup', params: { meetupId: meetup.id } }"
+        >{{ meetup.title }}</calendar-view-event
+      >
+    </template>
   </calendar-view>
 </template>
 
 <script>
 import CalendarView from './CalendarView';
 import CalendarViewEvent from './CalendarViewEvent';
+
+function makeDateString(date) {
+  return new Date(date).toDateString();
+}
 
 export default {
   name: 'MeetupsCalendar',
@@ -27,6 +33,21 @@ export default {
   components: {
     CalendarViewEvent,
     CalendarView,
+  },
+
+  computed: {
+    //митапы отсортированные по датам (только названия)
+    shedule() {
+      let shedule = {};
+      this.meetups.forEach((meetup) => {
+        let _date = makeDateString(meetup.date);
+        if (!shedule[_date]) {
+          shedule[_date] = [];
+        }
+        shedule[_date].push({ ...meetup });
+      });
+      return shedule;
+    },
   },
 };
 </script>
--- 10-architeture/01-RenderlessCalendar/components/RenderlessCalendar.vue
+++ 10-architeture/01-RenderlessCalendar/components/RenderlessCalendar.vue
@@ -1,7 +1,139 @@
 <script>
+/**
+ * Возвращает следующий день после текущего
+ * @param monthString {String} - Месяц год в формате MM.ГГ
+ * @return {Date} - первый день указаного месяца
+ */
+function getDateFromMonthString(monthString) {
+  return new Date(
+            +monthString.substr(3, 2) + 2000,
+            +monthString.substr(0, 2) - 1,
+          )
+}
+/**
+ * Возвращает следующий день после текущего
+ * @param date {Date} - текущая дата
+ * @return {Date} - следующий день
+ */
+function setNextDay(date) {
+  return new Date(date.setDate(date.getDate() + 1));
+}
+
+/**
+ * Возвращает первый день для отобра;ения в текущем представлении сетки
+ * @param date {Date} - первый день текущего месяца
+ * @return {Date} - объект даты
+ */
+function getFirstInGrid(date) {
+  let day = date.getDay() ? date.getDay() - 1 : 6; //день недели, с которого начинается месяц
+  return new Date(date.setDate(date.getDate() - day));
+}
+
+/**
+ * Возвращает сколько всего дней дол;но быть в сетке
+ * @param date - первый день текущего месяца
+ * @return {Number} - количество ячеек в сетке
+ */
+function getTotalDays(date) {
+  let month = date.getMonth(); //текущий месяц
+  let day = date.getDay() ? date.getDay() - 1 : 6; //день недели, с которого начинается месяц
+  let _daysInMonth = new Date(
+    new Date(new Date(date).setMonth(month + 1)).setDate(0),
+  ).getDate();
+  let _rest = (_daysInMonth + day) % 7 ? 7 - ((_daysInMonth + day) % 7) : 0;
+  let total = _daysInMonth + day + _rest;
+  return total;
+}
+
+/**
+ * Возвращает строку даты без часов / минут
+ * @param date {Date} - объект даты
+ * @return {string} - строка даты
+ */
+function makeDateString(date) {
+  return new Date(date).toDateString();
+}
 export default {
   name: 'RenderlessCalendar',
 
-  render(h) {},
+  data() {
+    return {
+      date: this.month
+        ? getDateFromMonthString(this.month)
+        : new Date(new Date().setDate(1)),
+    };
+  },
+
+  props: {
+    month: {
+      type: String,
+      default: null,
+      validator(value) {
+        let arr = Array.from({ length: 12 }, (v, k) => k + 1);
+        return arr.includes(+value.substr(0, 2)) && !isNaN(+value.substr(3, 2));
+      },
+    },
+  },
+
+  computed: {
+    //текущий месяц в строковом формате
+    currentMonth() {
+      return this.date
+        ? this.date.toLocaleString(navigator.language, {
+            year: 'numeric',
+            month: 'long',
+          })
+        : '';
+    },
+
+    //данные для ячеек сетки
+    days() {
+      let result = [];
+
+      let _current = new Date(this.date); //первый день текущего месяца
+      let _month = _current.getMonth(); //текущий месяц
+
+      _current = getFirstInGrid(_current); //понедельник первой недели месяца
+
+      let cells = getTotalDays(_current); //сколько всего ячеек в сетке текущего месяца
+
+      for (let cell = 1; cell <= cells; cell++) {
+        let day = {
+          num: _current.getDate(),
+          isActive: true,
+          date: makeDateString(_current),
+        };
+        if (_current.getMonth() != _month) day.isActive = false; //если не текущий месяц, ячейки неактивные
+
+        result.push(day); //запись данных ячейки в результат
+        _current = setNextDay(_current); //переход на следующую итерацию
+      }
+      return result;
+    },
+  },
+
+  methods: {
+    rewindMonth() {
+      let _date = new Date(this.date);
+      let _currentMonth = _date.getMonth();
+      this.date = new Date(_date.setMonth(_currentMonth - 1));
+    },
+
+    forwardMonth() {
+      let _date = new Date(this.date);
+      let _currentMonth = _date.getMonth();
+      this.date = new Date(_date.setMonth(_currentMonth + 1));
+    },
+  },
+
+  render() {
+    return this.$scopedSlots.default({
+      date: this.date,
+      currentMonth: this.currentMonth,
+      days: this.days,
+      rewindMonth: this.rewindMonth,
+      forwardMonth: this.forwardMonth,
+    });
+  },
 };
 </script>
